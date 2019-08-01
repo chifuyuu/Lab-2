d8102894309d8bdd7c2d81f43540d9901a8a30c1
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-08-01 09:11:42 +0800

    Update Activities.php

--- Activities.php
+++ Activities.php
@@ -50,10 +50,10 @@
 			
 			<ul>
 				</br>
-				<li><a href="Activity1.php" type="button" class="btn btn-outline-light btn-lg"></br>Activity 1</a></li>
-				<li><a href="Activity2.php" type="button" class="btn btn-outline-light btn-lg"></br>Activity 2</a></li>
-				<li><a href="Activity3.php" type="button" class="btn btn-outline-light btn-lg"></br>Activity 3</a></li>
-				<li><a href="#" type="button" class="btn btn-outline-light btn-lg"></br>Activity 4</a></li>
+				<li><a href="Activity1.php" type="button" class="btn btn-default btn-lg"></br>Activity 1</a></li>
+				<li><a href="Activity2.php" type="button" class="btn btn-default btn-lg"></br>Activity 2</a></li>
+				<li><a href="Activity3.php" type="button" class="btn btn-default btn-lg"></br>Activity 3</a></li>
+				<li><a href="#" type="button" class="btn btn-default btn-lg"></br>Activity 4</a></li>
 			</ul>	
 	</div>
 </header>
