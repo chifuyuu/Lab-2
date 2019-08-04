dac5327bd7b0733920cfcb1fc91fe2426c91b2ee
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-08-05 07:51:40 +0800

    Update Support.php

--- Support.php
+++ Support.php
@@ -43,7 +43,8 @@
 			<a href="https://www.facebook.com/ChifuyuGaming"><img src="https://scontent.fmnl17-1.fna.fbcdn.net/v/t1.0-9/66578783_391729278114538_1020921242451968000_n.jpg?_nc_cat=101&_nc_oc=AQkO-vmYmMQfoqOER2SMmKTPei1x2iBsm6ro63fAgO_KJ2iPn_tKDrArY3YxSG5IavA&_nc_ht=scontent.fmnl17-1.fna&oh=6a173d4537b7b32a154a19ca806d0608&oe=5DAE987A">
 			</a>
 			</div>
-			<br><br><br><br><br><h1>HELLO THERE KINDLY GIVE US A LIKE!</h1><br><br>
+			
+			</br></br></br><br><br><br><br><br><h1>HELLO THERE KINDLY GIVE US A LIKE!</h1><br><br>
 			<br><br><br><br><br><br><br><br><br><br><h1>AT CHIFUYU GAMING</h1><br><br>
 			<button class="button1" type="button" onclick="window.location.href='https://www.facebook.com/ChifuyuGaming'">Click Me!</button>
 		</div>
