05fa2c71d29fb516c463df97233779ce062fab60
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-08-05 08:32:50 +0800

    Add files via upload

---
+++ Q1.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="with=device-with, initial-scale=1">
+	<title>USER DES UI UX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel=stylesheet>
+</head>
+
+<!-- Navigation Bar-->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/fzFhKNg.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#"><img src="https://i.imgur.com/zYRC5Gx.png"></a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="About me.php">About</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="Support.php">Support Us</a>
+			</li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+
+<!--WELCOME-->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<hr>
+	<div class="col-12">
+		<p class="lead"> </br><h1>Reflection4</h1></br></br></p>
+			 
+			 </br>Ragsac, Paul Anthony</br>
+</br>SS 181
+
+</br>To start, our first quiz was unexpected, and I thought it was a good way to pointing on how important it is for a website having a very well-designed UI and UX.</br>  Taking this quiz, makes me realize that as an end user, we sometimes don’t read what we click on a website. We just click what we think is common or already experienced before on navigating through websites.</br> In the first few minutes of the quiz, I really don’t know what is happening. I just became pressured when everyone is already done and know what the steps navigating through it. </br>I can’t even past the first part of the quiz so I was already nervous if I will be the last one to pass the quiz. In the mid part, we already have insights on what to do to finish the quiz in a good time. It was to read every single word that is being required to proceed on to the next parts.</br>
+So, overall having this kind of interface in a website will mess someone’s time up and most importantly, it would give a lot of headaches to those who would be using it. </br>May even give inconsistent data since, end users cannot understand what they are doing in the website. We experienced that, creating a good UI and UX is important so that end users would not be bothered and had a lot of hard time as we did in our quiz  you need to put your all focus and mind into it and then you’ll get it.
+
+
+
+			</p>
+		</div>
+		
+		
+	</header>	
+</body>		
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="with=device-with, initial-scale=1">
+	<title>USER DES UI UX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel=stylesheet>
+</head>
+
+<!-- Navigation Bar-->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/fzFhKNg.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#"><img src="https://i.imgur.com/zYRC5Gx.png"></a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="About me.php">About</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="Support.php">Support Us</a>
+			</li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+
+<!--WELCOME-->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<hr>
+	<div class="col-12">
+		<p class="lead"> </br><h1>Quiz1</h1></br></br></p>
+
+<img src="https://trello-attachments.s3.amazonaws.com/5d0a24296ef37046aa76b12b/5d1012c7a2cb287ecc82cf3f/b22028fbd259bb8676eb2aaa1316d5fa/second.png">
+
+		
+		
+	</header>	
+</body>		
+</html>
