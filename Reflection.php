6a78214a1b7b226c9e4814ed30cb677db60e7c2f
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-08-05 08:08:42 +0800

    Add files via upload

---
+++ Reflection.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="with=device-with, initial-scale=1">
+	<title>USER DES UI UX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel=stylesheet>
+</head>
+
+<!-- Navigation Bar-->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/fzFhKNg.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#"><img src="https://i.imgur.com/zYRC5Gx.png"></a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="About me.php">About</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="Support.php">Support Us</a>
+			</li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+
+<!--WELCOME-->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<header>
+	<hr>
+	<div class="col-12">
+		
+		<p class="lead"> </br><h1><strong>This is the Activity Area</strong></h1></br></br> <h2>Click an Activity you want to view:)</h2></p>
+			
+			<ul>
+				</br>
+				<li><a href="R1.php"><h3>R1</h3></a></li>
+				<li><a href="R2.php"><h3>R2</h3></a></li>
+				<li><a href="R3.php"><h3>R3</h3></a></li>
+				<li><a href="R4.php"><h3>R4</h3></a></li>
+				<li><a href="Q1.php"><h3>Q1</h3></a></li>
+				</br></br>
+				<li><a href="index.php"><h3>GO BACK</h3></a></li>
+			</ul>	
+	</div>
+</header>
+</div>
+</div>
+
+	
+
