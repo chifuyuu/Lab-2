369c80a59951de793461abf408a1d9b6d0111bf0
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-08-05 08:11:41 +0800

    Add files via upload

---
+++ Activity5.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="with=device-with, initial-scale=1">
+	<title>USER DES UI UX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel=stylesheet>
+</head>
+
+<!-- Navigation Bar-->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand" href="#"><img src="https://i.imgur.com/fzFhKNg.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#"><img src="https://i.imgur.com/zYRC5Gx.png"></a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="About me.php">About</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="Support.php">Support Us</a>
+			</li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+
+<!--WELCOME-->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<hr>
+	<div class="col-12">
+		<p class="lead"> </br><h1>Activity 3</h1></br></br></p>
+			 
+</br>Asia Pacific College
+</br>3 Humabon Place, Magallanes Makati city
+
+
+</br>USERDES</br>
+
+</br>Activity 5: Initial Prototype – Log In Page</br>
+
+
+
+
+</br>Group members:</br>
+
+</br>Casabuena, Aliah Jezrine
+</br>Baello, Charl Alexander
+</br>Cagatan, Yeshua
+</br>Ragsac, Paul Anthony
+
+</br>LOG-IN PAGE
+
+
+
+</br>Input boxes:</br>
+</br>•	Email/username – to log-in, the user have to input his/her email or username which is used when she/he registered for Petagram.
+</br>•	Password – along with the email/username, the user also have to input his/her password for verification and account security. 
+</br>Links:</br>
+</br>•	forgot password – incase the user forgot his/her password, this link can be clicked to send a verification code to the user’s mobile number and/or email, so she can change her password. 
+</br>•	log-in with Gmail – if the user doesn’t want to create a new account and want to use her/his gmail account to be connected, the user can click this link to log-in his/her Gmail account. 
+
+</br>Buttons:</br>
+</br>•	log in – after providing the primary data (email/username and password), the user can click the log-in button so the provided data can be verified and the user can log-in to his/her account. 
+</br>•	sign up – incase the user is a first time user of petagram and wanted to have an own account, he/she can click this button to go to the sign-up page and fill-out the needed information. 
+
+</br>SIGN-UP PAGE</br>
+
+
+</br>Input boxes:</br>
+</br>•	Full name / Organization name: To register, the user needs to provide full name (if for personal use) or Organization name (if for organizational use). 
+</br>•	Username: the user will choose an available username to be use whenever he/she will log-in, username will also be displayed to their posts instead of their name for security.
+</br>•	Email / mobile number: This will be use for the user’s security and retrieval of account. 
+</br>•	Password: for security, password is required. Chosen password must be 8 characters and above to make sure that the password is strong. 
+
+</br>Check box:</br>
+</br>•	Check if for organization: this will check if the account that will be created is for personal use or organizational use. 
+
+</br>*If the user checked the box or specify that the account that will be created is for organization, the following field will be required to be filled out. 
+</br>•	Organization Head: This will ask for the name of the current head of the organization as a reference person and also for background checking.  
+</br>o	First name
+</br>o	Last name
+</br>•	Address: This will ask for the organization’s address as a proof and reference of the organization. To be specific this will include the following:
+</br>o	Street Address
+</br>o	City
+</br>o	State / Province
+</br>o	Postal Code
+</br>o	Country (drop down button)
+</br>•	Message: This field is for describing the organization process and their importance for the animals. This will also be one of the basis if the organization is fitted for the petagram’s advocacy for animals. 
+</br>Links:</br>
+</br>•	Requirements: when clicked, this link will show a page that contains all the requirements needed for the verification and confirmation of the organizational account. 
+</br>•	Petagram@email.com : this link will be use for the submission of the requirements. 
+</br>•	Terms and conditions: when clicked, this link will provide a pop-out box that contains the terms and condition of the petagram website. 
+</br>•	Log-in: this will link the user to the Log-in page, incase that he/she already have an account.
+</br>Buttons:</br>
+</br>•	Create account: after filling out the required fields, this button will be clicked to submit all the information and to proceed to verification and creation of the new account. 
+
+			</p>
+		</div>
+		
+		
+	</header>	
+</body>		
+</html>
